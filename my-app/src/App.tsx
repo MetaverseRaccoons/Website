ced5f0735d14a3bfda1e581301496d88982b94da
ben7-code <ben7.code@gmail.com>
2022-11-28 19:01:03 +0100

    initµ

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Kevin & Tristan zijn leuke backend developers</h2>
+      <h3>xoxo je frontendslaafje</h3>
+    </div>
+  );
+}
+
+export default App;
