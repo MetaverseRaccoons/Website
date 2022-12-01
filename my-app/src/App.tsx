c3b8265dd6c9ec8d02754b328431d2a29af5eb7a
ben7-code <ben7.code@gmail.com>
2022-12-01 15:00:53 +0100

    added tailwind & nav

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Kevin & Tristan zijn leuke backend developers</h2>
-      <h3>xoxo je frontendslaafje</h3>
+    <div>
+      <Navbar></Navbar>
+      <div className="App">
+        <h2 className="underline">Kevin & Tristan zijn leuke backend developers</h2>
+        <h3>xoxo je frontendslaafje</h3>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
     </div>
   );
 }
