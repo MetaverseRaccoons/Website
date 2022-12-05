2f7d8ab83013d6b9588ed52fa675d96654fbde00
benpoelmans <ben.poelmans123@gmail.com>
2022-12-05 14:54:58 +0100

    navbar update

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -2,11 +2,9 @@ const Navbar = () => {
   return (
     <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Rijsimulator
-          </span>
-        </a>
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          Rijsimulator
+        </span>
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
