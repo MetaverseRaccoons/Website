1cdd61d6a13bde3230c82e0554d759bb1530102d
benpoelmans <ben.poelmans123@gmail.com>
2023-02-15 17:25:10 +0100

    signup form update

--- my-app/src/pages/LoginPage.tsx
+++ my-app/src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react";
 import * as backend from "../apis/backend";
 
 const testfunc = async () => {
@@ -16,16 +17,59 @@ const testfunc = async () => {
   const createAccountResponse = await backend.createAccount(account);
 };
 
-/*
-<label className="w-full text-xl font-bold mt-4 mb-4">
-          Sign up
-        </label>
-*/
 const LoginPage = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [email, setEmail] = useState("");
+  const [national_registration_number, setNatRegNum] = useState("");
+  const [learner_or_instructor, setLearnerOrInstructor] = useState(false);
+  const [has_license, setHasLicense] = useState(false)
+  const [shareable, setShareable] = useState(false)
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      
+      if (username === '' || password === '' || password !== password2 || email === '' || national_registration_number === '') {
+        setError('Incomplete information')
+        return
+      }
+
+      const account: backend.CreateAccountRequest = {
+        username: username,
+        password1: password,
+        password2: password2,
+        email: email,
+        national_registration_number: national_registration_number,
+        is_learner: (learner_or_instructor === true) ? true : false,
+        is_instructor: (learner_or_instructor === false) ? true : false,
+        has_drivers_license: has_license,
+        is_shareable: shareable
+      }
+
+      const { user, refresh, access } = await backend.createAccount(account)
+
+      /*
+      const { refresh, access } = await backend.createToken({
+        username,
+        password,
+      });
+      window.localStorage.setItem("refresh", refresh);
+      window.localStorage.setItem("access", access);
+      window.location.href = "/home";
+      */
+    } catch(error) {
+      setError((error as Error).message);
+    }
+  };
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="h-1/2 w-2/3 rounded border-2 flex items-center justify-center">
-        <form className="w-full max-w-lg pt-12">
+      <div className="flex-shrink w-1/2 rounded border-2 flex items-center justify-center">
+        <form className="w-full max-w-lg pt-12" onSubmit={handleSubmit}>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -35,6 +79,7 @@ const LoginPage = () => {
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200"
                 id="naam"
                 type="text"
+                onChange={event => setUsername(event.target.value)}
               />
             </div>
             <div className="w-full md:w-1/2 px-3">
@@ -45,6 +90,7 @@ const LoginPage = () => {
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-200"
                 id="email"
                 type="email"
+                onChange={event => setEmail(event.target.value)}
               />
             </div>
           </div>
@@ -57,6 +103,7 @@ const LoginPage = () => {
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200"
                 id="password1"
                 type="password"
+                onChange={event => setPassword(event.target.value)}
               />
             </div>
             <div className="w-1/2 px-3">
@@ -67,6 +114,7 @@ const LoginPage = () => {
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200"
                 id="password2"
                 type="password"
+                onChange={event => setPassword2(event.target.value)}
               />
             </div>
           </div>
@@ -79,6 +127,7 @@ const LoginPage = () => {
               id="nationalregistrationnumber"
               type="text"
               placeholder="00.00.00-000.00"
+              onChange={event => setNatRegNum(event.target.value)}
             />
           </div>
           <div className="flex justify-center flex-wrap mb-2 ">
@@ -90,6 +139,7 @@ const LoginPage = () => {
               type="checkbox"
               value=""
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              onChange={event => setLearnerOrInstructor(event.target.checked)}
             />
           </div>
           <div className="flex justify-center flex-wrap mb-2 ">
@@ -101,6 +151,7 @@ const LoginPage = () => {
               type="checkbox"
               value=""
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              onChange={event => setLearnerOrInstructor(event.target.checked)}
             />
           </div>
           <div className="flex justify-center flex-wrap mb-6 ">
@@ -112,15 +163,19 @@ const LoginPage = () => {
               type="checkbox"
               value=""
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              onChange={event => setShareable(event.target.checked)}
             />
           </div>
-          <div className="flex justify-center mb-12">
+          <div className="flex flex-wrap justify-center mb-6">
             <button
-              onClick={testfunc}
+              type="submit"
               className="flex-shrink-0 w-1/2 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-normal font-bold border-4 text-white py-1 px-2 rounded"
             >
-              Login
+              Signup
             </button>
+          </div>
+          <div className="w-full justify-center items-center mt-2">
+            <p className="p-2">{error}</p>
           </div>
         </form>
       </div>
